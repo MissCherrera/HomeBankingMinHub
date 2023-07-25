30b66c1bac006b206138d8d17e7cb8c80c19f506
MissCherrera <cintia.ch.herrera@gmail.com>
2023-07-25 15:26:17 -0300

    Add project files.

---
+++ HomeBankingMinHub/Models/DbInitializer.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq;
+
+namespace HomeBankingMinHub.Models
+{
+    public class DbInitializer
+    {
+        public static void Initialize(HomeBankingContext context) 
+        {
+            if(!context.Clients.Any())
+            
+                //Se crea datos de prueba para la base de datos. arreglos
+            {
+                var clients= new Client []
+                {
+                    //Objeto de cliente
+                    new Client  {
+                        FirstName = "Cintia",
+                        LastName = "Herrera",
+                        Email = "cintia.ch.herrera@gmail.com",
+                        Password = "123456",
+                    },
+                    new Client  {
+                        FirstName = "Ramiro",
+                        LastName = "Olivencia",
+                        Email = "ramirOlivencia@gmail.com",
+                        Password = "87654321",
+                    },
+
+                };
+                //Para agregar nuevos datos a la base
+                foreach(var client in clients)
+                {
+                    // Se añade Clients al nuevo Cliente
+                    context.Clients.Add(client);
+                }
+                
+            }
+            // Por fuera de la logica, se guardan los datos almacenados, contexto
+            context.SaveChanges();
+
+        }
+    }
+}
---
+++ HomeBankingMinHub/Models/HomeBankingContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace HomeBankingMinHub.Models
+{
+    public class HomeBankingContext: DbContext
+    {
+        public HomeBankingContext(DbContextOptions<HomeBankingContext> options) : base(options)
+        { 
+            //dbset -variable- tabla en la base de datos
+        }
+        public DbSet <Client> Clients { get; set; }
+
+    }
+}
